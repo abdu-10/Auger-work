824ab8498127ecaad82c64fcc7efbacee2f1d900
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-04-26 11:12:58 +0300

    adds contact us

---
+++ src/components/Contact.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <>
+           
+
+     
+
+          
+<section class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+    <div class="text-center pb-12">
+        <h2 class="text-base font-bold text-blue-600">
+            Original is not necessarily the best
+        </h2>
+        <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-red-600">
+            Contact Us
+        </h1>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+            <div class="mb-8">
+                <img class="object-center object-cover rounded-full h-36 w-36" src="https://img.freepik.com/premium-vector/location-point-icon-vector-illustration_9999-17225.jpg?w=2000" alt="photo"/>
+            </div>
+            <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">Our Address</p>
+                <p class="text-base text-gray-400 font-normal">Industrial Area, Lusaka Road, opposite Total pertrol Station</p>
+            </div>
+        </div>
+        <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+            <div class="mb-8">
+                <img class="object-center object-cover rounded-full h-36 w-36" src="https://t3.ftcdn.net/jpg/05/46/69/68/360_F_546696877_RZ74IAylJACJOaCj09wpGQKb9o6PIE8K.jpg" alt="photo"/>
+            </div>
+            <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">Call us </p>
+                <p class="text-base text-gray-400 font-normal">+254720210844 </p>
+                <p class="text-base text-gray-400 font-normal">+254786111999 </p>
+                <p class="text-base text-gray-400 font-normal">+254712193912 </p>
+            </div>
+        </div>
+        <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+            <div class="mb-8">
+                <img class="object-center object-cover rounded-full h-36 w-36" src="https://t4.ftcdn.net/jpg/03/40/21/05/360_F_340210578_AREJV96D5YBg7xyvzs80SLUiJNW8eQKf.jpg" alt="photo"/>
+            </div>
+            <div class="text-center mb-90">
+                <p class="text-xl text-gray-700 font-bold mb-2">Our Timings</p>
+                <p class="text-base text-gray-400 font-normal">Mon-Fri: 8:00a.m-5:00p.m</p>
+                <p class="text-base text-gray-400 font-normal">Sat: 9:00a.m-2:00p.m</p>
+                <p class="text-base text-gray-400 font-normal">Sun: Closed</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+    </>
+  );
+}
+
+export default Contact;
